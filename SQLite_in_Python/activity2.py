fa0f3432ec61e1ba4fa5db48f6088a09a20c4d83
SamarthGlitch <samarthsajith123@gmail.com>
2026-03-17 18:33:20 +0530

    sqlinpython

--- SQLite_in_Python/activity2.py
+++ SQLite_in_Python/activity2.py
@@ -6,7 +6,7 @@ conn = sqlite3.connect(database)
 print('Opened data successfully')
 
 import pandas as pd
-tables = pd.read_sql("""SELECT
+tables = pd.read_sql("""SELECT *
                      FROM sqlite_master
                      WHERE type='table';""",conn)
 print(tables)
